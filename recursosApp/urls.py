5ed651a37a179adccfaee60978b410fc4bea59fa
Carla Bustos <carlabustos2008@gmail.com>
2026-09-11 11:47:57 -0300

    Agrega funcion editar recursos y mejora estilos

--- recursosApp/urls.py
+++ recursosApp/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-
+    path('eliminar/<int:id>/', views.eliminar_recurso, name='eliminar_recurso'),
+    path("editar/<int:id>/", views.editar_recurso, name="editar_recurso"),
 ]
--- recursosApp/views.py
+++ recursosApp/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import RecursoEducativo
 
 
 def inicio(request):
     recursos = RecursoEducativo.objects.all()
 
-    return render(request, 'recursosApp/inicio.html', {
-        'recursos' : recursos
+    busqueda = request.GET.get('busqueda')
 
+    if busqueda:
+        recursos = recursos.filter(comuna__icontains=busqueda)
+
+
+    return render(request, 'recursosApp/inicio.html', {
+        'recursos' : recursos,
+        'busqueda' : busqueda
     })
+
+def eliminar_recurso(request, id):
+    recurso = get_object_or_404(RecursoEducativo, id=id)
+
+    if request.method == 'POST':
+        recurso.delete()
+
+    return redirect('inicio')
+
+def editar_recurso(request, id):
+    recurso = get_object_or_404(RecursoEducativo, id=id)
+
+    if request.method == "POST":
+        recurso.nombre = request.POST.get("nombre")
+        recurso.descripcion = request.POST.get("descripcion")
+        recurso.region = request.POST.get("region")
+        recurso.comuna = request.POST.get("comuna")
+        recurso.categoria = request.POST.get("categoria")
+        recurso.gratuito = request.POST.get("gratuito") == "on"
+
+        recurso.save()
+
+        return redirect("inicio")
+
+    return render(request, "recursosApp/editar_recurso.html", {
+    "recurso": recurso
+})
